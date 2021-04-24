f698ebeec52a549c75fa5818792d3d07ecf7e1e6
matt russell <mrussell@cs.tufts.edu>
2021-04-24 16:12:12 -0400

    like->love

--- emoteToolScript.js
+++ emoteToolScript.js
@@ -116,7 +116,7 @@ function makeButtonContainer(size, x, y)
     var reactButtonDiv       = document.createElement('div');
     reactButtonDiv.innerHTML = reactButtonSVG;
     reactButtonDiv.setAttribute('id',"reactButtonSVG")
-    reactButtonDiv.onclick   = function() { handleReact('LIKE'); };
+    reactButtonDiv.onclick   = function() { handleReact('LOVE'); };
 
     var settingsDiv       = document.createElement('div');
     settingsDiv.innerHTML = settingsSVG;
