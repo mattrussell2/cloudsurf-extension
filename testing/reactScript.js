7498d6a604ada6033539bd109b3402d18b3cedd3
mshiel04 <mshiel04@eecs.tufts.edu>
2021-04-28 01:03:26 -0400

    percentage - based css overhaul

--- testing/reactScript.js
+++ testing/reactScript.js
@@ -4,32 +4,40 @@ disliked = false;
 document.getElementsByClassName("like-button")[0].addEventListener("click", function() {
     if (disliked) {
         document.getElementsByClassName("dislike-button")[0].src = "../images/DislikeUnclicked.svg";
+        document.getElementsByClassName("dislike-button")[0].classList.remove('clicked-button');
         document.getElementsByClassName("like-button")[0].src = "../images/LikeClicked.svg";
+        document.getElementsByClassName("like-button")[0].classList.add('clicked-button');
         disliked = false;
         liked = true;
     }
     else if (liked) {
         document.getElementsByClassName("like-button")[0].src = "../images/LikeUnclicked.svg";
+        document.getElementsByClassName("like-button")[0].classList.remove('clicked-button');
         liked = false;
     }
     else {
         document.getElementsByClassName("like-button")[0].src = "../images/LikeClicked.svg";
+        document.getElementsByClassName("like-button")[0].classList.add('clicked-button');
         liked = true;
     }
 });
 document.getElementsByClassName("dislike-button")[0].addEventListener("click", function() {
     if (liked) {
         document.getElementsByClassName("like-button")[0].src = "../images/LikeUnclicked.svg";
+        document.getElementsByClassName("like-button")[0].classList.remove('clicked-button');
         document.getElementsByClassName("dislike-button")[0].src = "../images/DislikeClicked.svg";
+        document.getElementsByClassName("dislike-button")[0].classList.add('clicked-button');
         disliked = true;
         liked = false;
     }
     else if (disliked) {
         document.getElementsByClassName("dislike-button")[0].src = "../images/DislikeUnclicked.svg";
+        document.getElementsByClassName("dislike-button")[0].classList.remove('clicked-button');
         disliked = false;
     }
     else {
         document.getElementsByClassName("dislike-button")[0].src = "../images/DislikeClicked.svg";
+        document.getElementsByClassName("dislike-button")[0].classList.add('clicked-button');
         disliked = true;
     }
 });
